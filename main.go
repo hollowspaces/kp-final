ad9334ea6eae7d5da571e2a1c479b7ebca2782a8
Puji <49268936+hollowspaces@users.noreply.github.com>
2024-07-23 18:39:15 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,23 +1,23 @@
 package main
 
 import (
-	"kp-final/db"
-	"kp-final/routes"
-
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+    "kp-final/db"
+    "kp-final/routes"
+    "github.com/labstack/echo/v4"
+    "github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
-	// Inisialisasi database
-	if err := db.InitDB(); err != nil {
-		panic("Gagal menginisialisasi database: " + err.Error())
-	}
+    // Inisialisasi database
+    if err := db.InitDB(); err != nil {
+        panic("Gagal menginisialisasi database: " + err.Error())
+    }
 
-	e := echo.New()             // Inisialisasi server Echo
-	e.Use(middleware.Logger())  // Middleware
-	e.Use(middleware.Recover()) // Inisialisasi rute
-	routes.SetupRoutes(e)
+    e := echo.New()             // Inisialisasi server Echo
+    e.Use(middleware.Logger())  // Middleware
+    e.Use(middleware.Recover()) // Inisialisasi rute
 
-	e.Start(":8080") // Start server pada port 8080
+    routes.SetupRoutes(e)
+
+    e.Start(":8080") // Start server pada port 8080
 }
