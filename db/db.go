cd18fb9b3918e23a9b55c2390dcac95872a10526
Puji <49268936+hollowspaces@users.noreply.github.com>
2024-07-23 18:33:38 +0700

    Update db.go

--- db/db.go
+++ db/db.go
@@ -3,19 +3,19 @@ package db
 import (
 	"kp-final/models"
 
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func InitDB() error {
-	// Konfigurasi koneksi ke database MySQL
-	dsn := "root@tcp(127.0.0.1:3306)/kp_final_puji?charset=utf8mb4&parseTime=True&loc=Local"
+	// Konfigurasi koneksi ke database PostgreSQL
+	dsn := "host=dpg-cqfp2kaju9rs73c1dr2g-a user=postgresql_puji_user password=88UMCaUBA2LdzGINsOqtDSFIjeXxzP1T dbname=postgresql_puji port=5432 sslmode=disable TimeZone=Asia/Jakarta"
 	var err error
 
-	// Membuka koneksi ke database menggunakan driver MySQL
-	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	// Membuka koneksi ke database menggunakan driver PostgreSQL
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("Gagal terhubung ke database: " + err.Error())
 	}
